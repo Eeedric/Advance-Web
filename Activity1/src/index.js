f5e8dbb9585d421871e61362e9b851c9f9e9910e
Eedric <edricpulgarinass@gmail.com>
2022-03-09 20:30:57 -0600

    Activity1

---
+++ Activity1/src/index.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Edric L. Pulgarinas</h1>
+
+<style>
+h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+
+<h1> I'M EDRIC PULGARINAS!</h1>
+<div>
+COURSE & YEAR: BSIT - 3
+</div>
+<div>
+AGE: 21
+</div>
+<div>
+ADDRESS: Purok 4 Looc Proper Plaridel
+</div>
+</h1>
+
+  
+</div>
+`;
